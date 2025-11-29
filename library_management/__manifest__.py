554c3072d4b1dd9088552fd6e0b9592d6c541604
vionadjunaidy <vionadjunaidy@gmail.com>
2025-11-29 12:52:15 +0700

    first commit

---
+++ library_management/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Library Management System',
+    'version': '1.0',
+    'summary': 'A module to manage library operations including books, members, and loans.',
+    'description': 'This module provides functionalities to manage books, library members, and track book loans and returns.',
+    'author': 'Viona',
+    'depends': ['base'],
+    'data': [
+        'views/library_book_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
